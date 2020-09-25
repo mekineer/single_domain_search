0602fa4dd2e0ebe245d7a5a69ff689b7f0091242
mekineer <36555751+mekineer@users.noreply.github.com>
2020-09-25 02:14:34 -0400

    Update util.py

--- util.py
+++ util.py
@@ -1,12 +1,16 @@
 import logging
+import datetime
+import os
 
+home_path,file_name_ = os.path.split(os.path.realpath(__file__))
+LOGFILE = os.path.join(home_path,str(datetime.datetime.now()).replace(' ','_')+'_scdn.log')
 
 def get_logger(logger_name, level=logging.DEBUG):
     logger = logging.getLogger(logger_name)
     logger.setLevel(level)
     logging.basicConfig(
         filemode="w",
-        filename="scdn.log",
+        filename=LOGFILE,
         format="[%(asctime)s][%(levelname)s]: %(message)s"
     )
     return logger
