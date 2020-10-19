1352bc15c91d8ec454118806d3037a4b5c19b335
webexpert <pksalo6@gmail.com>
2020-10-19 14:38:28 +0530

    models

--- frontend/app/models.py
+++ frontend/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class QueryInfo(models.Model):
+	query = models.CharField(max_length=200)
+	query_date = models.DateTimeField(auto_now_add=True)
+	processed_count = models.IntegerField(default=0)
+
+	class Meta:
+		db_table = 'query_info'
+
+class VisitedUrls(models.Model):
+	url = models.CharField(max_length=500)
+	begin_date = models.DateTimeField(auto_now_add=True)
+	mark = models.IntegerField(default=0)
+
+	class Meta:
+		db_table = 'visited_urls'
