040cdc0c7e2ef019b13bc8c447016c0ce75c5e7c
webexpert <pksalo6@gmail.com>
2020-10-23 17:05:22 +0530

    made the website dynamic

--- frontend/app/views.py
+++ frontend/app/views.py
@@ -6,16 +6,22 @@ def search(request):
 	
 	if request.method == 'POST':
 		query = request.POST.get('search')
-		resp = requests.get("http://127.0.0.1:8000/api/queryapi/?query={}".format(query))
-		data = resp.json()
+		# print(query)
+		# resp = requests.get("http://127.0.0.1:8000/api/queryapi/?query={}".format(query))
+		# data = resp.json()
 		# print(data)
 		cb1 = request.POST.get('cb1')
 		cb2 = request.POST.get('cb2')
 		cb3 = request.POST.get('cb3')
 		res = QueryInfo.objects.filter(query__icontains=query)
 		# print(res)
-		return render(request,'app/search.html',{'data':data})
-	return render(request,'app/search.html')
+		# print(res)
+		if query:
+			return render(request,'google_sec.html',{'data':res})
+		else:
+			return render(request,'search-eng.html')
+		# return render(request,'app/search.html',{'data':data})
+	return render(request,'search-eng.html')
 
 
 
