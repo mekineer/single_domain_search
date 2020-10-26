daf3837ea8e3f3764b7a4404b507a1885b71ec6e
webexpert <pksalo6@gmail.com>
2020-10-26 14:35:25 +0530

    backend integration with front end

--- frontend/app/views.py
+++ frontend/app/views.py
@@ -10,17 +10,12 @@ def search(request):
 		# resp = requests.get("http://127.0.0.1:8000/api/queryapi/?query={}".format(query))
 		# data = resp.json()
 		# print(data)
-		cb1 = request.POST.get('cb1')
-		cb2 = request.POST.get('cb2')
-		cb3 = request.POST.get('cb3')
 		res = QueryInfo.objects.filter(query__icontains=query)
-		# print(res)
 		# print(res)
 		if query:
 			return render(request,'google_sec.html',{'data':res,'query':query})
 		else:
 			return render(request,'search-eng.html')
-		# return render(request,'app/search.html',{'data':data})
 	return render(request,'search-eng.html')
 
 
